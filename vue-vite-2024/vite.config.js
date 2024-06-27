8c65700f3438e8c3121a477ae73b0d0114b3080a
Alex Nio <volpenio@gmail.com>
2024-06-27 17:07:10 +0500

    add vue-vite-2024

---
+++ vue-vite-2024/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public',
+  define: {
+    'process.env': {},
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/app/scss/global.scss";`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
