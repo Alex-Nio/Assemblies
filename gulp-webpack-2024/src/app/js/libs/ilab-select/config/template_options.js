e62b908bfb46fa62ded2003543037863060454b3
Alex Nio <volpenio@gmail.com>
2024-06-27 16:19:33 +0500

    add gulp-webpack-2024

---
+++ gulp-webpack-2024/src/app/js/libs/ilab-select/config/template_options.js
@@ -0,0 +1,56 @@
+// Функция для создания объектов опций
+const setDropdownOptions = (type, placeholder, icon, selectedId) => ({
+  type,
+  data: [],
+  input: { placeholder, icon, selectedId },
+});
+
+// Создание объектов опций
+const default_template = setDropdownOptions(
+  'default',
+  'Выбрать',
+  'icon-caret24'
+);
+
+const sorting_template = setDropdownOptions(
+  'default',
+  'Сортировать по',
+  'icon-caret24'
+);
+
+const edition_template = setDropdownOptions(
+  'default',
+  'Добавить редакцию',
+  'icon-caret24'
+);
+
+const search_template = setDropdownOptions(
+  'search',
+  'Поиск по тегам',
+  'icon-search24'
+);
+
+const headerSearch_template = setDropdownOptions(
+  'search',
+  'Найти решение..',
+  'icon-search24'
+);
+
+const inn_template = setDropdownOptions('search', 'ИНН *', '');
+
+const request_template = setDropdownOptions(
+  'search',
+  'Выбрать тему',
+  'icon-caret24'
+);
+
+// Экспорт шаблона
+export const templates = {
+  default: default_template,
+  search: search_template,
+  inn: inn_template,
+  sorting: sorting_template,
+  headerSearch: headerSearch_template,
+  selectEdition: edition_template,
+  request: request_template,
+};
---
+++ gulp-webpack-2024/src/template/Main/public/main.js
@@ -0,0 +1,6 @@
+//* Libs
+import './../../../app/js/utils/handlers.js';
+//? Common Components
+import './../../../components/Common/footer/main-footer/footer.js';
+//? Shared Components
+//? Chapter Unique Components
